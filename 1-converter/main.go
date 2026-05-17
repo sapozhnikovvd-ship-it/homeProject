5aac758c253c7c29a76fdd627ddf0f798934dd03
sapozhnikovvd-ship-it <sapozhnikovvd@gmail.com>
2026-05-17 13:38:12 +0300

    feat: added inputConvertInfo and convertCurrency

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,42 @@
 package main
 
-func main() {
+import "fmt"
+
+// Форматированный ввод пользователем информации для конверсии валют
+func inputConvertInfo() (amount float64, sourceCurrency string, finalCurrency string) {
+	fmt.Scanf("%f %s %s", &amount, &sourceCurrency, &finalCurrency)
+	return
+}
+
+func convertCurrency(amount float64, sourceCurrency string, finalCurrency string) float64 {
 	const USDToEUR float64 = 0.932
 	const USDToRUB float64 = 70.323
 	const EURToRUB float64 = USDToRUB / USDToEUR
+
+	switch sourceCurrency {
+	case "RUB":
+		if finalCurrency == "USD" {
+			return amount / USDToRUB
+		} else if finalCurrency == "EUR" {
+			return amount / EURToRUB
+		}
+	case "USD":
+		if finalCurrency == "RUB" {
+			return amount * USDToRUB
+		} else if finalCurrency == "EUR" {
+			return amount * USDToEUR
+		}
+	case "EUR":
+		if finalCurrency == "USD" {
+			return amount / USDToEUR
+		} else if finalCurrency == "RUB" {
+			return amount * EURToRUB
+		}
+	}
+
+	return amount
+}
+
+func main() {
+	fmt.Printf("Result: %f\n", convertCurrency(inputConvertInfo()))
 }
