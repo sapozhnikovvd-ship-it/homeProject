cd6e24e085b543c52c8ad8c087c2cd5f3c9dac05
sapozhnikovvd-ship-it <sapozhnikovvd@gmail.com>
2026-08-31 21:21:41 +0300

    refactor: edited the input function inputConvertInfo

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,10 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // Форматированный ввод пользователем информации для конверсии валют
 func inputConvertInfo() (amount float64, sourceCurrency string, finalCurrency string) {
-	fmt.Scanf("%f %s %s", &amount, &sourceCurrency, &finalCurrency)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Input amount: ")
+
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Invalid input amount: %v\nTry again\n", err)
+				continue
+			}
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+		amountLocal, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Printf("Invalid input amount: %v\nTry again\n", err)
+			continue
+		}
+
+		if amountLocal < 0 {
+			fmt.Printf("Invalid input: the amount must be equal to zero or be positive\nTry again\n")
+			continue
+		}
+		amount = amountLocal
+		break
+	}
+
+	for {
+		fmt.Print("Input source currency (RUB, EUR, USD): ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Invalid input source currency: %v\nTry again\n", err)
+				continue
+			}
+		}
+
+		sourceCurrency = strings.TrimSpace(scanner.Text())
+
+		if sourceCurrency != "RUB" && sourceCurrency != "EUR" && sourceCurrency != "USD" {
+			fmt.Printf("Invalid input: the source currency must be RUB, EUR or USD\nTry again\n")
+			continue
+		}
+		break
+	}
+
+	for {
+		fmt.Print("Input final currency (RUB, EUR, USD): ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Invalid input final currency: %v\nTry again\n", err)
+				continue
+			}
+		}
+
+		finalCurrency = strings.TrimSpace(scanner.Text())
+
+		if finalCurrency != "RUB" && finalCurrency != "EUR" && finalCurrency != "USD" {
+			fmt.Printf("Invalid input: the final currency must be RUB, EUR or USD\nTry again\n")
+			continue
+		}
+		break
+	}
 	return
 }
 
@@ -32,6 +100,8 @@ func convertCurrency(amount float64, sourceCurrency string, finalCurrency string
 		} else if finalCurrency == "RUB" {
 			return amount * EURToRUB
 		}
+	default:
+		return amount
 	}
 
 	return amount
